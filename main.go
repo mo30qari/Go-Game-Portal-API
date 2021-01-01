bacc503c9e1bbebeb84695f944bccd5b90a79b0e
mostafa qari <mo30qari@gmail.com>
2021-01-01 12:45:34 +0330

    Learned about tags in struct to validate them

--- main.go
+++ main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 type User struct {
-	Username string
-	Avatar string
-	Password string
+	Username string `json:"username"`
+	Avatar string `json:"avatar"`
+	Password string `json:"password"`
 }
 
 type Game struct {
-	Name string
-	Genre Genre
-	Icon string
-	Link string
-	Owner User
+	Name string `json:"username"`
+	Genre Genre `json:"genre"`
+	Icon string `json:"icon"`
+	Link string `json:"link"`
+	Owner User `json:"owner"`
 }
 
 type Genre struct{
-	Name string
+	Name string `json:"name"`
 }
 
 func main(){
-
+	//must be deleted
+	i, _ := json.MarshalIndent(Genre{Name: "Shooter"}, "", " ")
+	fmt.Println(string(i))
+	//
 	router := mux.NewRouter()
 	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/register/{username}/{password}/{confirm}/{Avatar}", register).Methods("POST")
 
 	http.ListenAndServe(":8080", router)
+
+
 
 }
